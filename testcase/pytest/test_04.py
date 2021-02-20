933c67bf8e5398f70da46b474ed96d5eab2c7bcc
weilutao <252306298@qq.com>
2021-02-20 18:33:29 +0800

    15

--- testcase/pytest/test_04.py
+++ testcase/pytest/test_04.py
@@ -38,3 +38,20 @@ data3 = (
 @pytest.mark.parametrize('dic', data3)
 def test3(dic):
     print(dic)
+
+
+# 通过pytest.param()来定义参数
+# id是对参数的说明，id的值可以自定义，只要方便每个用例是干什么的即可
+data4 = [
+    pytest.param(1, 2, 3, id='(a+b):pass'),
+    pytest.param(2, 4, 5, id='(a+b):fail')
+]
+
+
+def add(a, b):
+    return a+b
+
+
+@pytest.mark.parametrize('a, b, expect', data4)
+def test4(a, b, expect):
+    assert add(a, b) == expect
