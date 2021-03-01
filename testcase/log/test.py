9a3b242ecc9b2654c53f8e3f3123bd469832aa5d
weilutao <252306298@qq.com>
2021-03-01 21:45:01 +0800

    16

---
+++ testcase/log/test.py
@@ -0,0 +1,10 @@
+import logging
+
+# 设置级别
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug('debug')
+logging.info('info')
+logging.warning('warning')
+logging.error('error')
+logging.critical('critical')
--- testcase/pytest/test_07.py
+++ testcase/pytest/test_07.py
@@ -21,17 +21,24 @@ class TestEditPage():
     '''编辑页面'''
 
     @allure.story("这是一个xxx的用例")
-    def test_1(self, login):
+    def test_3(self, login):
         '''用例描述：先登录，再去执行xxx'''
         step_1()
         step_2()
-        print("yyy")
+        print("xxx")
 
     @allure.story("打开页面a")
-    def test_2(self, login):
+    def test_4(self, login):
         '''用例描述：先登录，再执行yyy'''
         print('yyy')
 
-        
+
+if __name__ == '__main__':
+    ''' 生成测试报告必须在命令行执行
+        pytest --alluredir ./reports test_07.py
+        allure serve ./reports 启动allure 查看报告
+    '''
+    pytest.main(['--alluredir', './reports', 'test_07.py'])
+
 
 
