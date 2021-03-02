a66a9cd7ab7232ee3fecb05446a8812145b288aa
weilutao <252306298@qq.com>
2021-03-02 09:21:03 +0800

    16

--- util/ready_course.py
+++ util/ready_course.py
@@ -24,11 +24,12 @@ class OA_Course(object):
         self.driver.find_element_by_xpath('/html/body/div/div[2]/div/div/div[1]/div/div[2]/button').click()
 
     def select_class(self):
+        name = '魏禄涛'
         try:
             WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/ul[1]/li[4]/div'))).click()
             sleep(1)
             WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.PARTIAL_LINK_TEXT, '中心班级列表'))).click()
-            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//button/span[contains(text(),'魏禄涛')]"))).click()
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//button/span[contains(text(),'%s')]" % name))).click()
         except Exception as e:
             print(e)
 
