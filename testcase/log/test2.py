70d1231d2a74ec4829c22a56aa732f80aec1e098
weilutao <252306298@qq.com>
2021-03-03 23:12:12 +0800

    17

--- testcase/log/test2.py
+++ testcase/log/test2.py
@@ -3,7 +3,7 @@ import logging
 my_format = '%(asctime)s-%(levelname)s-%(message)s'
 
 logging.basicConfig(
-    filename='E:/selenium_project01/logs/my.log',
+    filename='C:/python3.7.0/selenium_project01/logs/my.log',
     level=logging.INFO,
     format=my_format
 )
