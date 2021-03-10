5851eaea99b92ecd094111af439bd5aa1fe2f6b3
weilutao <252306298@qq.com>
2021-03-10 08:33:07 +0800

    16

---
+++ testcase/log/test3.py
@@ -0,0 +1,12 @@
+import logging
+import logging.handlers
+import datetime
+
+logger = logging.getLogger('mylogger')
+logger.setLevel(logging.DEBUG)
+
+rt_handler = logging.handlers.TimedRotatingFileHandler('all.log', when='mignight', interval=1, backupCount=7,
+                                                       atTime=datetime.time(0, 0, 0, 0))
+rt_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+f_handler =
