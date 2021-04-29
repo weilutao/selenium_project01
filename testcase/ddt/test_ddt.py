ea5af863f2d0a3b19b766d133c17cb8b473bc473
weilutao <252306298@qq.com>
2021-04-29 21:29:42 +0800

    18

--- testcase/ddt/test_ddt.py
+++ testcase/ddt/test_ddt.py
@@ -8,6 +8,12 @@ def get_data():
 
 @ddt
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        print('---------------------------')
+
+    def tearDown(self) -> None:
+        print('---------------------------')
+
     # 读取元组数据--单组元素
     @data(1, 2, 3)
     def test1(self, value):
@@ -29,7 +35,30 @@ class MyTestCase(unittest.TestCase):
     def test4(self, value):
         print(value)
 
+    # 字典
+    @data({'name':'tom', 'age':20}, {'name':'kite', 'age':30})
+    def test5(self,value):
+        print(value)
+
+    # 字典-拆分
+    @data({'name':'tom', 'age':20}, {'name':'kite', 'age':30})
+    @unpack
+    def test6(self, name, age):
+        print(name, age)
+
+    # 变量或者方法调用
+    testdata = [{'name':'tom', 'age':20}, {'name':'kite', 'age':30}]
+
+    # @data(*testdata)
+    @data(get_data())
+    def test7(self, value):
+        print(value)
+
+    # 读文件
+    @file_data(os.getcwd() + '/test.json')
+    def test8(self, value):
+        print(value)
 
 
 if __name__ == '__main__':
-    unittest.main
+    unittest.main()
